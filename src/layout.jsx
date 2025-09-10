42d4711c608af867bff68f5c05f1213b9edc9cbf
pedroviscz <pedrolucasalmeida7@hotmail.com>
2025-09-10 12:09:35 -0300

    feat: cria roteamento inicial com rotas index (home) e login.

---
+++ src/layout.jsx
@@ -0,0 +1,19 @@
+import './rootlayout.css'
+import { Outlet} from 'react-router-dom'
+
+export function RootLayout() {
+  return(
+    <div>
+      {/* isso aqui vai virar um componente header depois */}
+      <header>
+        <nav></nav>
+      </header>
+
+      {/* aqui vai o conteúdo das páginas que tem header e footer */}
+      <Outlet />
+
+      {/* isso aqui vai virar um componente footer depois */}
+      <footer></footer>
+    </div>
+  )
+}
