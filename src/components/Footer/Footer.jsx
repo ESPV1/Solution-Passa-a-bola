675943a32860a15f9c89b073ed869a0686480253
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-11 09:37:06 -0300

    feat:Criação base da estrutura do Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+function Footer(){
+    return(
+        <footer>
+            <section>
+                <h2>Fale Conosco</h2>
+                <span>passaabola@dominio.com</span>
+                <div>
+                </div>
+                    <span>IC1</span>
+                    <span>IC2</span>
+                    <span>IC3</span>
+            </section>
+            <section>
+                <h2>Links rápidos</h2>
+                <nav>
+                    <ul>
+                        <li>Sobre nós</li>
+                        <li>Perguntas frequentes - FAQ</li>
+                        <li>Politica de Pirivacidade</li>
+                        <li>Termos de uso</li>
+                        <li>Cards de Jogadoras</li>
+                        <li>Eventos</li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <h2>Passa a Bola</h2>
+                <span>Todos os direitos Reservados</span>
+            </section>
+        </footer>
+    )
+
+}
+
+export default Footer
