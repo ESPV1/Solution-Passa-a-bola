93b3e36b7d16f5952514e11ec169d02dcd66cc53
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-10 21:25:23 -0300

    feat: Criando componente Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+        <header>
+            <h2>Passa a bola</h2>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Jogadoras</li>
+                    <li>Times</li>
+                    <li>Quadras</li>
+                </ul>
+                <button>Login</button>
+                <button>Cadastre-se</button>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
