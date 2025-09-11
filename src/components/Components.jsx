ec2c571769b377732a57673f23da1995371b9bba
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-10 23:04:00 -0300

    stlye: Estilo basico do Header

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,17 +1,17 @@
 function Header(){
     return(
-        <header>
+        <header className="w-full px-4 py-2 grid [grid-template-columns:0.5fr_1fr] border border-gray-300 items-center">
             <h2>Passa a bola</h2>
-            <nav>
-                <ul>
+            <nav className="flex items-center justify-around">
+                <ul className="flex gap-6">
                     <li>Home</li>
                     <li>Jogadoras</li>
                     <li>Times</li>
                     <li>Quadras</li>
                 </ul>
-                <ul>
-                    <li>Login</li>
-                    <li>Cadastrar-se</li>
+                <ul className="flex gap-6">
+                    <li className="px-8 py-2 border border-gray-300">Login</li>
+                    <li className="px-8 py-2 border border-gray-300">Cadastre-se</li>
                 </ul>
             </nav>
         </header>
