8a61e30c3ab63e8a8db74df02a9636c4eaf703ea
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-10 23:03:19 -0300

    feat:Criação estrutura base Header

---
+++ src/components/Components.jsx
@@ -0,0 +1,21 @@
+function Header(){
+    return(
+        <header>
+            <h2>Passa a bola</h2>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Jogadoras</li>
+                    <li>Times</li>
+                    <li>Quadras</li>
+                </ul>
+                <ul>
+                    <li>Login</li>
+                    <li>Cadastrar-se</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
