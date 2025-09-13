17f4b0c90dc4990f61693c5afe016fd2f70661aa
Gabriel Amara <gabriel.amara@hotmail.com>
2025-09-12 21:43:45 -0300

    Style footer completo

--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -1,3 +1,6 @@
+import { FaInstagram } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer className="grid grid-cols-3 px-8 py-8 bg-[#383838] text-white">
@@ -7,13 +10,13 @@ export default function Footer() {
                     <span>passaabola@dominio.com</span>
                 </div>
                 <div className="flex gap-2">
-                    <span>IC1</span>
-                    <span>IC2</span>
-                    <span>IC3</span>
+                    <span><FaInstagram size="2em"/></span>
+                    <span><FaTwitter size="2em"/></span>
+                    <span><FaYoutube size="2em"/></span>
                 </div>
             </section>
             <section className="flex flex-col gap-2 border-r-2">
-                <h2 className="text-2xl ml-px">Links rápidos</h2>
+                <h2 className="text-2xl ml-[2rem]">Links rápidos</h2>
                 <nav className="flex justify-around">
                     <ul className="flex flex-col gap-3">
                         <li>Sobre nós</li>
